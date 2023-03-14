9d0dc409d16985495146ae27ea61d9a69eaa21f7
agoorveni <agoorveni@gmail.com>
2023-03-14 14:49:29 +0530

    Veni Git

---
+++ src/veni/org/VeniGit.java
@@ -0,0 +1,17 @@
+package veni.org;
+
+public class VeniGit {
+
+	public static void main(String[] args) {
+	
+		
+		
+		System.out.println("Hi i am Veni");
+		
+		System.out.println(1);
+		
+		
+	}
+	
+	
+}
